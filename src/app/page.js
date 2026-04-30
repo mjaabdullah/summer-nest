ce0f2f897bd6e8d5a64541768e82b5518032b642
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-04-30 13:19:05 +0600

    Initial project setup

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/shared/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <h1>Your Summer Essentials Hub</h1>
+    </>
+  );
+}
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>navBar</div>;
+};
+
+export default Navbar;
